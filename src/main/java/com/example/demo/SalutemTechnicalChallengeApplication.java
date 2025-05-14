54d082fa7c9d8f532decaa6545cb4c4252e85785
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2025-05-14 10:54:18 -0300

    Initial files

---
+++ src/main/java/com/example/demo/SalutemTechnicalChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalutemTechnicalChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalutemTechnicalChallengeApplication.class, args);
+	}
+
+}
